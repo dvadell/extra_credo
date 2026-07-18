5f6c2df31575361f73d3fefc3116da84fd105d7c
Diego M. Vadell <diego.m.vadell@gmail.com>
2026-07-18 21:23:25 +0000

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExtraCredo.MixProject do
   def project do
     [
       app: :extra_credo,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
